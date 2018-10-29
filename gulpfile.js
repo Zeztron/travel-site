d48eb5f8df1fb24ccc3afe8bc617e286a956e107
Zeztron <pateltharsh@gmail.com>
2018-10-29 14:46:34 -0400

    Added CSS Architecture and normalize.css

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp         = require("gulp"),
     postcss      = require("gulp-postcss"),
     cssvars      = require("postcss-simple-vars"),
     nested       = require("postcss-nested"),
+    cssImport    = require("postcss-import"),
     autoprefixer = require("autoprefixer");
 
 gulp.task("default", function(done) {
@@ -17,17 +18,17 @@ gulp.task("html", function(done) {
 
 gulp.task("styles", function(done) {
   return gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest("./app/temp/styles"));
 });
 
 gulp.task("watch", function(done) {
-
-
+  // watch the html
   watch("./app/index.html", function() {
     gulp.start("html");
   });
 
+  // watch the css
   watch("./app/assets/styles/**/*.css", function() {
     gulp.start("styles");
   });
