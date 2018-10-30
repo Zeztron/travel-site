702fc84c5e4e3bf3c3d7ea7915e9d678f1fa5e68
Zeztron <pateltharsh@gmail.com>
2018-10-30 19:57:42 -0400

    Added webpack automation with gulp and automatic refresh with browsersync

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 var $ = require("jquery");
 var Person = require("./modules/Person");
 
+alert("ABC 123");
+
 var john = new Person("John Doe", "blue");
 john.greet();
 var jane = new Person("Jane Smith", "green");
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -22,8 +22,17 @@ gulp.task("watch", function(done) {
     gulp.start("cssInject");
   });
 
+  // watch the js
+  watch("./app/assets/scripts/**/*.js", function() {
+    gulp.start("scriptsRefresh");
+  });
+
 });
 
 gulp.task("cssInject", ["styles"],function() {
   return gulp.src("./app/temp/styles/styles.css").pipe(browserSync.stream());
+});
+
+gulp.task("scriptsRefresh", ["scripts"], function () {
+  browserSync.reload();
 });
