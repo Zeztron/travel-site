726e4bddd23d369e2bd0ffbe2eadc2188516d982
Zeztron <pateltharsh@gmail.com>
2018-10-29 20:18:09 -0400

    Implementing mobile first design

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,13 +1,14 @@
 var gulp         = require("gulp"),
     postcss      = require("gulp-postcss"),
     cssvars      = require("postcss-simple-vars"),
+    mixins       = require("postcss-mixins"),
     nested       = require("postcss-nested"),
     cssImport    = require("postcss-import"),
     autoprefixer = require("autoprefixer");
 
 gulp.task("styles", function(done) {
   return gulp.src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .on("error", function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit("end");
