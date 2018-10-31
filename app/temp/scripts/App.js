9863649f17d16289f8cf4931f4494dcf03d120a5
Zeztron <pateltharsh@gmail.com>
2018-10-31 09:36:13 -0400

    Made Reveal Module recyclable, and styed the reveal to have a zoom out effect -- Finished reveal scroll

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -99,10 +99,15 @@ var _RevealOnScroll = __webpack_require__(3);
 
 var _RevealOnScroll2 = _interopRequireDefault(_RevealOnScroll);
 
+var _jquery = __webpack_require__(2);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var mobileMenu = new _MobileMenu2.default();
-var revealOnScroll = new _RevealOnScroll2.default();
+new _RevealOnScroll2.default((0, _jquery2.default)(".feature-item"), "85%");
+new _RevealOnScroll2.default((0, _jquery2.default)(".testimonial"), "60%");
 
 /***/ }),
 /* 1 */
@@ -10440,10 +10445,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var RevealOnScroll = function () {
-  function RevealOnScroll() {
+  function RevealOnScroll(els, offset) {
     _classCallCheck(this, RevealOnScroll);
 
-    this.itemsToReveal = (0, _jquery2.default)(".feature-item, .testimonial");
+    this.itemsToReveal = els;
+    this.offsetPercentage = offset;
     this.hideInitially();
     this.createWaypoints();
   }
@@ -10456,6 +10462,7 @@ var RevealOnScroll = function () {
   }, {
     key: "createWaypoints",
     value: function createWaypoints() {
+      var that = this;
       this.itemsToReveal.each(function () {
         var currentItem = this;
         new Waypoint({
@@ -10463,7 +10470,7 @@ var RevealOnScroll = function () {
           handler: function handler() {
             (0, _jquery2.default)(currentItem).addClass("reveal-item--is-visible");
           },
-          offset: "85%"
+          offset: that.offsetPercentage
         });
       });
     }
